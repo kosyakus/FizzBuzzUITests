2e111c142c6cd3d6bea6c5d0407018dcab9c98a2
Unknown <kosyak_@mail.ru>
2018-06-21 16:48:51 +0200

    created the brain of the app and tests for every func

---
+++ FizzBuzzTests/BrainTests.swift
@@ -0,0 +1,64 @@
+//
+//  BrainTests.swift
+//  FizzBuzzTests
+//
+//  Created by Наталья Синицына on 21.06.2018.
+//  Copyright © 2018 Наталья Синицына. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class BrainTests: XCTestCase {
+    
+    let brain = Brain()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testIsDivisibleByThree() {
+        let brain = Brain()
+        let result = brain.isDivisibleByThree(number: 3)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsDivisibleByFive() {
+        let brain = Brain()
+        let result = brain.isDivisibleByFive(number: 10)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsDivisibleByFifteen() {
+        let brain = Brain()
+        let result = brain.isDivisibleByFifteen(number: 15)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testSayFizz() {
+        let result = brain.check(number: 3)
+        XCTAssertEqual(result, "Fizz")
+    }
+    
+    func testSayBuzz() {
+        let result = brain.check(number: 5)
+        XCTAssertEqual(result, "Buzz")
+    }
+    
+    func testSayFizzBuzz() {
+        let result = brain.check(number: 15)
+        XCTAssertEqual(result, "FizzBuzz")
+    }
+    
+    func testSayNumber() {
+        let result = brain.check(number: 1)
+        XCTAssertEqual(result, "1")
+    }
+    
+}
